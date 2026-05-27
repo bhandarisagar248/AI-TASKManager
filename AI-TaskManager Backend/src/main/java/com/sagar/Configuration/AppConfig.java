10dc35d26819d7543b2c1d42224d0846f25283db
Sagar Bhandari <143873798+bhandarisagar248@users.noreply.github.com>
2026-05-27 09:32:24 +0545

    fix bug

--- AI-TaskManager Backend/src/main/java/com/sagar/Configuration/AppConfig.java
+++ AI-TaskManager Backend/src/main/java/com/sagar/Configuration/AppConfig.java
@@ -1,4 +1,4 @@
-package net.javaguides.ems.Configuration;
+package com.sagar.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
